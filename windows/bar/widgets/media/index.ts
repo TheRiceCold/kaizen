4ac59cd108b8b91bd9f3789da53b2f91cdf74437
TheRiceCold <dalewaltergh@gmail.com>
2024-03-17 00:42:03 +0800

    fix: bar media pause back to player

--- windows/bar/widgets/media/index.ts
+++ windows/bar/widgets/media/index.ts
@@ -9,32 +9,32 @@ const { preferred, action } = options.media
 const { bars, width, height } = options.bar.media.cava
 const getPlayer = () => mpris.getPlayer(preferred.value) || mpris.players[0] || null
 
-const StackContent = Widget.Stack({
-  className: 'stack',
-  transition: 'slide_up_down',
-  children: {
-    player: Widget.Box(),
-    cava: Widget.Box({
-      hpack: 'center',
-      className: 'cava-container',
-      child: cava({ bars: bars * width, width, height }),
-    })
-  },
-})
-
 export default () => {
   let player = getPlayer()
+
+  const StackContent = Widget.Stack({
+    className: 'stack',
+    transition: 'slide_up_down',
+    children: {
+      player: Widget.Box(),
+      cava: Widget.Box({
+        hpack: 'center',
+        className: 'cava-container',
+        child: cava({ bars: bars * width, width, height }),
+      })
+    },
+  }).hook(mpris, self => {
+    if (player.play_back_status === 'Paused')
+      self.shown = 'player'
+  })
+
   return BarButton({
     className: 'media',
     child: Widget.Box([
       PlayIcon(player),
       StackContent,
     ]),
-    onClicked: () => {
-      player.playPause()
-      if (player.play_back_status === 'Playing')
-        StackContent.shown = 'player'
-    },
+    onClicked: player.playPause,
     onSecondaryClick: action.bind(),
     onScrollUp: () => StackContent.shown = 'player',
     onScrollDown: () => StackContent.shown = 'cava',
