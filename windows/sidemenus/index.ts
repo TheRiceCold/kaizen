44de1423458b4b8e57b74fa617a367fd8aceb13c
TheRiceCold <dalewaltergh@gmail.com>
2024-04-05 19:47:01 +0800

    chore: setup sideleft apis menu

--- windows/sidemenus/index.ts
+++ windows/sidemenus/index.ts
@@ -1,5 +1,17 @@
+import Apis from './apis'
 import DateMenu from './datemenu'
 import QuickSettings from './quicksettings'
+
+const left = Widget.Window({
+  name: `sideleft`,
+  className: 'sideleft',
+  anchor: ['top', 'left'],
+  child: Widget.Box({
+    vertical: true,
+    css: 'padding: 2px;',
+    children: [ Apis ]
+  }),
+})
 
 const right = Widget.Window({
   name: `sideright`,
