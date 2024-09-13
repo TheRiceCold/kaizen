a76ddfa773aaa17be784d33164ebcb6559be2ebc
TheRiceCold <dalewaltergh@gmail.com>
2024-09-14 03:57:22 +0800

    style & refactor: dashboard sidebar

--- ags/desktop/fullscreen/dashboard/index.ts
+++ ags/desktop/fullscreen/dashboard/index.ts
@@ -7,7 +7,9 @@ import Avatar from './Avatar'
 import options from 'options'
 import { capitalize } from 'lib/utils'
 
-const stackItems = [
+const activePage = Variable('home')
+
+const pages = [
   { name: 'home', icon: '', content: Home },
   { name: 'tasks', icon: '', content: Tasks },
   { name: 'journal', icon: '', content: Widget.Box() },
@@ -19,33 +21,34 @@ const stackItems = [
 
 const Stack = Widget.Stack({
   className: 'page',
+  shown: activePage.bind(),
   transition: 'slide_up_down',
-  children: stackItems.reduce((acc, item) => {
+  children: pages.reduce((acc, item) => {
     acc[item.name] = item.content
     return acc
-  }, {})
+  }, {}),
 })
 
-const TabButton = index => Widget.Button({
-  hpack: 'start',
-  cursor: 'pointer',
-  onClicked() { Stack.shown = stackItems[index].name },
-  label: stackItems[index].icon+'  '+capitalize(stackItems[index].name),
+const TabButton = (i: number)=> Widget.Button({
+  xalign: 0, cursor: 'pointer',
+  onClicked() { activePage.value = pages[i].name },
+  child: Widget.Label(`${pages[i].icon}  ${capitalize(pages[i].name)}`),
+}).hook(activePage, self => {
+  self.toggleClassName('active', pages[i].name === activePage.value)
 })
 
 const Sidebar = Widget.Revealer({
+  revealChild: true,
   className: 'sidebar',
   transition: 'slide_right',
   transitionDuration: options.transition,
-  child: Widget.Box({
-    vertical: true,
-    children: [
-      Widget.Label({
-        className: 'user',
-        label: `${capitalize(Utils.USER)}'s Dashboard`
-      })
-    ].concat(Array.from({ length: stackItems.length }, (_, i) => i).map(TabButton))
-  })
+  child: Widget.Box({vertical: true},
+    Widget.Label({
+      className: 'user',
+      label: ` ${capitalize(Utils.USER)}'s Dashboard `
+    }),
+    ...Array.from({ length: pages.length }, (_, i) => i).map(TabButton)
+  )
 })
 
 const MenuButton = Widget.Button({
@@ -54,32 +57,37 @@ const MenuButton = Widget.Button({
   vpack: 'start',
   cursor: 'pointer',
   className: 'menu-button',
-  onClicked() { Sidebar.revealChild = !Sidebar.revealChild }
+  onClicked() {
+    Sidebar.revealChild = !Sidebar.revealChild
+  }
 })
 
-const Header = Widget.Overlay({
-  passThrough: true,
-  className: 'header',
-  overlays: [ MenuButton, Avatar ],
-  child: Widget.Box(
-    { className: 'cover', vertical: true },
-    Widget.Box({
-      className: 'cover-img',
-      css: wallpaper.bind('wallpaper').as(
-        (wp: string) => `
-        min-height: 200px;
-        background-size: cover;
-        background-image: url('${wp}');`)
-    }),
-  )
-})
-
-const Main = Widget.Box({ vertical: true, className: 'main' }, Header, Stack)
+const Dashboard = Widget.Box(
+  { className: 'dashboard' }, Sidebar,
+  Widget.Box(
+    { vertical: true, className: 'main' },
+    Widget.Overlay({
+      passThrough: true,
+      className: 'header',
+      overlays: [ MenuButton, Avatar ],
+      child: Widget.Box(
+        { className: 'cover', vertical: true },
+        Widget.Box({
+          className: 'cover-img',
+          css: wallpaper.bind('wallpaper').as(
+            (wp: string) => `
+            min-height: 200px;
+            background-size: cover;
+            background-image: url('${wp}');`)
+        }),
+      )
+    }), Stack)
+)
 
 export default Widget.Window({
   visible: false,
+  child: Dashboard,
   name: 'dashboard',
   keymode: 'on-demand',
   anchor: ['top', 'bottom', 'right', 'left'],
-  child: Widget.Box({ className: 'dashboard' }, Sidebar, Main)
 }).keybind('Escape', () => App.closeWindow('dashboard'))
